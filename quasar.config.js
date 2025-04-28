b3977bfeafe30779058c1947d0fc948a604e7b01
Sogol Eizadshenass <sogol.izad@gmail.com>
2025-04-28 12:07:41 -0400

    set a public path on quasar config

--- quasar.config.js
+++ quasar.config.js
@@ -46,7 +46,7 @@ export default defineConfig((ctx) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/vue-stocks-dashboard',
       // analyze: true,
       // env: {},
       // rawDefine: {}
